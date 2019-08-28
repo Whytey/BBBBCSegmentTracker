7f0ab26ce6867cd2c110aa04902f438ed07bb11d
Whytey <Whytey@users.noreply.github.com>
2019-08-28 21:11:33 +1000

    Latest build of polymer FE.  Features now supported: View members Connect new members.

--- main.py
+++ main.py
@@ -125,33 +125,6 @@ def all_other_routes(the_path):
     return app.send_static_file(the_path)
 
 
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if request.method == 'GET':
-#         return render_template('index.html', segment={"name": "Test Segment", "id": 1234})
-#
-#     return redirect(url_for('index'))
-#
-#
-# @app.route('/m', methods=['GET', 'POST'])
-# def m():
-#     if request.method == 'GET':
-#         members = Member.objects.all()
-#
-#         return render_template('members.html', members=members)
-#
-#     return redirect(url_for('m'))
-#
-#
-# @app.route('/challenges', methods=['GET', 'POST'])
-# def challenges():
-#     if request.method == 'GET':
-#         challenges = Challenge.objects.all()
-#
-#         return render_template('challenges.html', challenges=challenges)
-#
-#     return redirect(url_for('challenges'))
-#
 #
 # @app.route('/attempts/<challenge>', methods=['GET', 'POST'])
 # def attempts(challenge):
@@ -168,50 +141,6 @@ def all_other_routes(the_path):
 #     return redirect(url_for('attempts'))
 #
 #
-# @app.route('/connect', methods=['GET', 'POST'])
-# def connect():
-#     if request.method == 'GET':
-#         state = request.args.get('state')
-#         if state == 'response':
-#             # We've returned from the strava authentication call.
-#             error_msg = request.args.get('error', None)
-#             if error_msg:
-#                 # We didn't actually authenticate.
-#                 return render_template('connect.html', error=error_msg)
-#
-#             # We must have been sucessful
-#             code = request.args.get('code')
-#
-#             client = Client()
-#             token_response = client.exchange_code_for_token(client_id=config.strava_client_id,
-#                                                             client_secret=config.strava_client_secret,
-#                                                             code=code)
-#             access_token = token_response['access_token']
-#             refresh_token = token_response['refresh_token']
-#             expires_at = token_response['expires_at']
-#
-#             client.access_token = access_token
-#             client.refresh_token = refresh_token
-#             client.token_expires_at = expires_at
-#
-#             athlete = client.get_athlete()
-#
-#             # Member.objects.create(last_name=athlete.lastname, first_name=athlete.firstname, id=athlete.id,
-#             #                       refresh_token=refresh_token, access_token=access_token,
-#             #                       access_token_expiry=expires_at)
-#             m = Member.add(athlete, refresh_token, access_token, expires_at)
-#
-#
-#             flash('You were successfully connected.  Welcome {}!'.format(athlete.firstname))
-#
-#             return redirect(url_for('index'))
-#
-#         client = Client()
-#         authorize_url = client.authorization_url(config.strava_client_id, request.base_url, state="response")
-#
-#         return render_template('connect.html', target_url=authorize_url)
-#
-#     return redirect(url_for('connect'))
 
 
 if __name__ == '__main__':
